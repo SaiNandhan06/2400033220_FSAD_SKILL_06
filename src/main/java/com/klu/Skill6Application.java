65afa378d5215604af91c3b048ea71e1cb0885f1
SaiNandhan06 <sainandhanmucharla@gmail.com>
2026-04-18 08:32:59 +0530

    Finalized code fixes and application configurations

--- src/main/java/com/klu/Skill6Application.java
+++ src/main/java/com/klu/Skill6Application.java
@@ -3,7 +3,7 @@ package com.klu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.klu", "Controller"})
 public class Skill6Application {
 
   public static void main(String[] args) {
